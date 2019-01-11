25b9ae1c932e4f3f39787b35f0f1196b5a6d3caf
yoosangkeon <enter_maintanance@naver.com>
2019-01-11 15:22:09 +0900

    [DEV] Add TextView Placeholder
    - Message를 담을 수 있는 textView에 Placeholder추가
    - GBS확인을 위한 컨트롤러 추가

---
+++ CBACampProject/CBACampProject/SearchGBSViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchGBSViewController.swift
+//  CBACampProject
+//
+//  Created by JUDY on 2019. 1. 11..
+//  Copyright © 2019년 mac. All rights reserved.
+//
+
+import UIKit
+
+class SearchGBSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CBACampProject/CBACampProject/SendMessageViewController.swift
+++ CBACampProject/CBACampProject/SendMessageViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class SendMessageViewController: UIViewController {
+class SendMessageViewController: UIViewController, UITextViewDelegate {
     var dbRef : DatabaseReference!
     
     @IBOutlet weak var textAuthor: UITextField!
@@ -28,6 +28,12 @@ class SendMessageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textMessage.text = "하고 싶은 말을 남겨주세요"
+        textMessage.textColor = UIColor.lightGray
+        textMessage.font = UIFont(name: "verdana", size: 13.0)
+        textMessage.returnKeyType = .done
+        textMessage.delegate = self
+        
         dbRef = Database.database().reference()
 
         SendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
@@ -39,6 +45,28 @@ class SendMessageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "하고 싶은 말을 남겨주세요" {
+            textView.text = ""
+            textView.textColor = UIColor.black
+            textView.font = UIFont(name: "verdana", size: 18.0)
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = "하고 싶은 말을 남겨주세요"
+            textView.textColor = UIColor.lightGray
+            textView.font = UIFont(name: "verdana", size: 13.0)
+        }
+    }
 
     /*
     // MARK: - Navigation
