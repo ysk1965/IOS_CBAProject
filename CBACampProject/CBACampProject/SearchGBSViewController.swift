2a57e4d46869c5a4f05dbbac14d90005fba12a5e
yoosangkeon <enter_maintanance@naver.com>
2019-01-23 04:01:46 +0900

    [DEV] Update GBS System
    - GBS시스템 뼈대 제작 -> 추가 개발 필요

--- CBACampProject/CBACampProject/SearchGBSViewController.swift
+++ CBACampProject/CBACampProject/SearchGBSViewController.swift
@@ -9,22 +9,67 @@
 import UIKit
 import FirebaseAuth
 
-class SearchGBSViewController: UIViewController {
-
-    var allURLs = [String]()
-    var allTitles = [String]()
-    var allmembersDicts = [[String : Any]]()
+struct MyGBS: Codable {
+    //let id : String?
+    //let retreat_id : String?
+    let gbsLevel : String?
     
+    let leader: UserInfo?
+    let members : [UserInfo]
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        //id = try values.decodeIfPresent(String.self, forKey: .id)
+        //retreat_id = try values.decodeIfPresent(String.self, forKey: .retreat_id)
+        gbsLevel = try values.decodeIfPresent(String.self, forKey: .gbsLevel)
+        
+        leader = try values.decodeIfPresent(UserInfo.self, forKey: .leader)!
+        members = try values.decodeIfPresent([UserInfo].self, forKey: .members)!
+    }
+}
+
+struct UserInfo: Codable{
+    let name : String?
+    let age : String?
+    let campus : String?
+    let mobile : String?
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decodeIfPresent(String.self, forKey: .name)
+        age = try values.decodeIfPresent(String.self, forKey: .age)
+        campus = try values.decodeIfPresent(String.self, forKey: .campus)
+        mobile = try values.decodeIfPresent(String.self, forKey: .mobile)
+    }
+}
+
+class SearchGBSViewController: UIViewController {
+    var gbsLevel : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //parseURL(theURL : "http://cba.sungrak.or.kr:8888/getMyInfo/" + (Auth.auth().currentUser?.uid)!)
-        
-        jsonParser(theURL : "http://cba.sungrak.or.kr:8888/getMyInfo/" + (Auth.auth().currentUser?.uid)!)
-        
-        print("http://cba.sungrak.or.kr:8888/getMyInfo/" + (Auth.auth().currentUser?.uid)!)
-
+        if(Auth.auth().currentUser != nil){
+            let url = "http://cba.sungrak.or.kr:8888/getGBSInfo/" + (Auth.auth().currentUser?.uid)!
+            let urlObj = URL(string: url)
+            
+            URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+                guard let data = data else {return}
+                
+                do {
+                    let decoder = JSONDecoder()
+                    var myGBSs = try decoder.decode(MyGBS.self, from: data)
+                    
+                    self.gbsLevel = myGBSs.gbsLevel!
+                    
+                    print(self.gbsLevel!)
+                } catch{
+                    print(url)
+                    print("We got an error", error.localizedDescription)
+                }
+                
+                }.resume()
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -33,90 +78,6 @@ class SearchGBSViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func jsonParser(theURL:String){
-        let strJSonString = theURL
-        let oJsonDataT:Data? = strJSonString.data(using: .utf8)
-        
-        if let oJsonData = oJsonDataT
-        {
-            var oJsonDictionaryT:[String:Any]?
-            oJsonDictionaryT = try! JSONSerialization.jsonObject(with: oJsonData, options: []) as! [String:Any]
-            
-            if let oJsonDictionary = oJsonDictionaryT
-            {
-                if let name = oJsonDictionary["name"],
-                    let campus = oJsonDictionary["campus"]
-                {
-                    print("name = \(name)")
-                    print("name = \(campus)")
-                }
-            }
-        }
-    }
-    
-    func parseURL(theURL:String){
-        let url = URL(string: theURL)
-        URLSession.shared.dataTask(with:url!) { (data, response, error) in
-            if error != nil{
-                print("didn`t work, \(String(describing: error))")
-                
-                DispatchQueue.main.asyncAfter(deadline: .now()){
-                    
-                    // just deal
-                }
-            } else{
-                do{
-                    let parseData = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
-                    
-                    //self.allmembersDicts.removeAll() // replace data
-                
-                    for (key, value) in parseData{
-                        //print("\(key) --- \(value)")
-                        
-                        if(key == "members"){
-                        
-                            if let membersArray: [[String : Any]] = value as? [[String : Any]]{
-                                
-                                self.allmembersDicts = membersArray
-                                
-                                print("is array of dictionaries")
-                                
-                                for dict in membersArray{
-                                    for (key, value) in dict{
-                                        if(key == "url"){
-                                            print("url is \(value)")
-                                            self.allURLs.append(value as! String)
-                                        } else if (key == "title"){
-                                            print("title is \(value)")
-                                            self.allURLs.append(value as! String)
-                                        } else if (key == "source"){
-                                            if let sourceDict:[String : Any] = value as? [String : Any]{
-                                                print (sourceDict)
-                                                
-                                                for (key, value) in sourceDict{
-                                                    if (key == "id"){
-                                                        print(value)
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                
-                            }
-                        }
-                    }
-                } catch let error as NSError{
-                    print(error)
-                    
-                    DispatchQueue.main.asyncAfter(deadline: .now()){
-                        
-                    }
-                    // just deal
-                }
-            }
-        }.resume()
-    }
 
     /*
     // MARK: - Navigation
