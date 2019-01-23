2be10e9c113408505a180b46097539f582b3c246
yoosangkeon <enter_maintanance@naver.com>
2019-01-24 03:32:23 +0900

    [DEV] Add Pinch Zoom
    - Sidebar에서 핀치 줌으로 이미지 확대

--- CBACampProject/CBACampProject/SideBarViewController.swift
+++ CBACampProject/CBACampProject/SideBarViewController.swift
@@ -455,57 +455,19 @@ class SideBarViewController: UIViewController, UIScrollViewDelegate, SideMenuDel
             MainView.bounceIn(from: .right)
             Check = false
         }
- 
-        /*
-        let pinchRecogniezer = UIPinchGestureRecognizer(target: self, action : #selector(pinchAction(_ :)))
         
-        scrollView.addGestureRecognizer(pinchRecogniezer)
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(sender:)))
+        view.addGestureRecognizer(pinch)
         
-        var pinch = UIPinchGestureRecognizer(target: self, action: Selector("handlePinchGesture:"))
-        self.scrollView.addGestureRecognizer(pinch)
-        */
-        
-        //self.scrollView.minimumZoomScale = 1.0
-        //self.scrollView.maximumZoomScale = 5.0
-        
-        /*
-        scrollView.alwaysBounceVertical = false
-        scrollView.alwaysBounceHorizontal = false
-        scrollView.minimumZoomScale = 1.0
-        scrollView.maximumZoomScale = 2.0
-        scrollView.delegate = self
-        */
-        
-        /*
-        var doubleTapRecognizer = UITapGestureRecognizer(target : self, action : #selector(scrollViewDoubleTapped(_:)))
-        doubleTapRecognizer.numberOfTapsRequired = 2
-        doubleTapRecognizer.numberOfTouchesRequired = 1
-        scrollView.addGestureRecognizer(doubleTapRecognizer)
-        
-        let scrollViewFrame = scrollView.frame
-        let scaleWidth = scrollViewFrame.width / scrollView.contentSize.width
-        let scaleHeight = scrollViewFrame.height / scrollView.contentSize.height
-        let minScale = min(scaleWidth, scaleHeight)
-        scrollView.minimumZoomScale = minScale
-        
-        scrollView.maximumZoomScale = 1.0
-        scrollView.zoomScale = minScale
-         */
     }
     
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView?{
-        return self.imageView
-    }
-    
-    
-    @objc func pinchAction(_ sender : UIPinchGestureRecognizer){
-        scrollView.transform = scrollView.transform.scaledBy(x: sender.scale, y: sender.scale)
+    @objc func handlePinch(sender: UIPinchGestureRecognizer) {
+        guard sender.view != nil else { return }
         
-        sender.scale = 1.0
-    }
-    
-    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return self.imageView
+        if sender.state == .began || sender.state == .changed {
+            sender.view?.transform = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale))!
+            sender.scale = 1.0
+        }
     }
     
     @objc func Logout(_ sender:UIButton){
@@ -541,39 +503,4 @@ class SideBarViewController: UIViewController, UIScrollViewDelegate, SideMenuDel
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /*
-    @available(iOS 2.0, *)
-    public func viewForZooming(in ScrollView: UIScrollView)-> UIView?{
-        return self.imageView
-    }
-    
-    @objc func scrollViewDoubleTapped(_ recognizer: UITapGestureRecognizer){
-        let pointInView = recognizer.location(in : imageView)
-        
-        var newZoomScale = scrollView.zoomScale * 1.5
-        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
-        
-        let scrollViewSize = scrollView.bounds.size
-        let w = scrollViewSize.width / newZoomScale
-        let h = scrollViewSize.height / newZoomScale
-        let x = pointInView.x - (w / 2.0)
-        let y = pointInView.y - (h / 2.0)
-        
-        let rectToZoomTo = CGRect(x: x, y: y, width: w, height: h)
-        
-        scrollView.zoom(to: rectToZoomTo, animated: true)
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
