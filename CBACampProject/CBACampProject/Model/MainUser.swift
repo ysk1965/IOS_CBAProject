7ded4db35ef8f7af0048695532489d4ecf48bbbe
ysk1965 <enter_maintanance@naver.com>
2019-07-10 12:26:14 +0900

    [DEV] make singleton pattern
    - 싱글톤 패턴 구현 중

--- CBACampProject/CBACampProject/Model/MainUser.swift
+++ CBACampProject/CBACampProject/Model/MainUser.swift
@@ -34,3 +34,45 @@ class MainUser {
         self.grade = grade
     }
 }
+
+struct ButtonType(){
+    // type : Image or Segue or Call or URL
+    var type : String?
+    var iconName : String? // sidebar는 text
+    // image : imageName
+    // segue : segueIdentifier
+    // call : phoneNumber
+    // URL : URL Link
+    
+    // SIDEBAR : ImageName
+    var controlValue : String?
+}
+
+
+// Make SingleTonClass
+class AgencySingleton {
+    static let shared = AgencySingleton()
+    
+    var AgencyTitle : String?
+    var viewBannerName : String? // View 배너 이미지 이름
+    var sidebarBannerName : String? // 사이드 바 배너 이미지 이름
+    var topTagImageName : String? // View 중앙 상단 이미지 이름
+    
+    // SIDEBAR 메뉴에 들어갈 내용들 추가
+    var sidebar_setting : Array<ButtonType> = []
+    // MainView에 들어갈 내용들 추가
+    var bottombar_setting : Array<ButtonType> = []
+    
+    // 쓸지 고민 중...
+    private init() {}
+    
+    private init(AgencyTitle : String, viewBannerName : String, sidebarBannerName : String, topTagImageName : String, sidebar_setting: Array<ButtonType>, bottombar_setting : Array<ButtonType>){
+        self.AgencyTitle = AgencyTitle
+        self.viewBannerName = image_name
+        self.sidebarBannerName = sidebarBannerName
+        self.topTagImageName = topTagImageName
+        
+        self.sidebar_setting = sidebar_setting
+        self.bottombar_setting = view_setting
+    }
+}
